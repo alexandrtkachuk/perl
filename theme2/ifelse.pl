87c43d166e9b1ccb65ffef48a3c5a5832d1abaf2
aitkachuk <alexandr@localhost.localdomain>
2015-02-19 21:19:01 +0200

    theme 2

---
+++ theme2/ifelse.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+
+$endl="\n";
+
+#for string
+# == eq
+# != ne
+# <  lt
+# >  gt
+# <= le
+# >= ge
+
+if(7==7 )
+{
+    print '7=7'.$endl;
+}
+
+if('test' eq 'tset' )
+{
+#
+}
+else
+{
+print 'test!=tset'.$endl;
+}
+
+
+
+
+
---
+++ theme2/test.plx
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+
+$endl="\n";
+
+$a=5;
+$b=8;
+$c=$a*$b;
+print $c.$endl; 
+
+
+
+print 'f=', 4 , 'B=', 17 , 'f*B=',4*17, $endl;
+
+#print "<b>Hello</b>, world!";
+
