1d25e2f7f48940ef66045198cfdd5aa3e2e55c80
aitkachuk <alexandrtai@gmail.com>
2015-03-17 18:42:40 +0200

    eh

--- cours/task1/test.pl
+++ cours/task1/test.pl
@@ -1,4 +1,30 @@
 #!/usr/bin/perl
 
-use Data::Dumper;
-print "test";
+print "good\n";
+$count=0;$count++;$result='Итого='.$count."\n";1;
+
+print $result;
+
+
+print "$_ " for (1 ... 10);
+
+print "\n";
+
+@arr=(0 ... 14);
+
+print "$_ " for @arr;
+
+print "\n";
+
+$" = ':'; # установим разделитель элементов
+print "@arr";
+
+#print "\n".(1...99)[0];
+
+
+print "\n".shift @arr;
+print "\n";
+while (my $first = shift @arr) { # пока @array не опустеет
+    print "Обработан элемент $first.";
+    print "Осталось ", scalar @arr, " элементов\n";
+}
